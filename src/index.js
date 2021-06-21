b2f674fb584b1e45f0d6edccfce52eacc65e1893
Abhinav9550 <63183306+Abhinav9550@users.noreply.github.com>
2021-06-21 11:13:27 +0530

    task Completed for Chezubha

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+//import Container from "react-bootstrap";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Categories from "./components/categories";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <Categories />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
